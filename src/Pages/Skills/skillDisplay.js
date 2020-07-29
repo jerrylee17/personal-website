79d625a38ec714db085cf2acd696b7a45ce3446c
jerrylee17 <jerrylee1157@gmail.com>
2020-07-28 17:07:05 -0700

    Added spinner

---
+++ src/Pages/Skills/skillDisplay.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import './Skills.css'
+
+import anime from 'animejs/lib/anime.es.js';
+
+export default function skillDisplay(props) {
+
+  return (
+    <>
+
+    </>
+  )
+}
